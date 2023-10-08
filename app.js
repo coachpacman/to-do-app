e3ce76876083622f41cc95a76f3f7134ba0d8534
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-08 12:19:22 -0700

    clear input field and create checkbox with list item

--- app.js
+++ app.js
@@ -6,13 +6,25 @@
 // }
 
 function addToDoItem() {
+  //get text from input field
   var toDoValue = document.getElementById("to-do-input").value
   
+  //create list item using input field value
   var listItem = document.createElement("li");
   var text = document.createTextNode(`todo: ${toDoValue}`);
   listItem.appendChild(text);
 
+  //create checkbox for list item
+  var checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+
+  //add list item to html
   var itemsList = document.getElementById("itemsList");
   itemsList.appendChild(listItem);
+  itemsList.appendChild(checkbox);
+  
+  //clear input field
+  document.getElementById("to-do-input").value=""
+
 }
 
