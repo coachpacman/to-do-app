ee27c9e455a8fe8049d833ce9bcd7a9369e2f0b2
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-08 01:57:07 -0700

    added javascript variable to input value for dynamic list items

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function getInputValue() {
-  var toDoValue = document.getElementById("to-do-input").value
-  console.log(toDoValue)
-}
+// var toDoValue;
+
+// function getInputValue() {
+//   toDoValue = document.getElementById("to-do-input").value
+//   console.log(toDoValue)
+// }
 
 function addToDoItem() {
+  var toDoValue = document.getElementById("to-do-input").value
+  
   var listItem = document.createElement("li");
-  var text = document.createTextNode("list item");
+  var text = document.createTextNode(`todo: ${toDoValue}`);
   listItem.appendChild(text);
 
   var itemsList = document.getElementById("itemsList");
