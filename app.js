f90fe4b9b22d7cc7a16168f0742d9829d8d55934
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-10 14:38:55 -0700

    remove strikeThrough() and add event listener in addToDoItem()

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ function addToDoItem() {
   var checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox", "id", "test-input", "onClick", "strikeThrough()");
   checkbox.setAttribute("id", "test-input");
-  checkbox.setAttribute("onClick", "strikeThrough()");
+  // checkbox.setAttribute("onClick", "strikeThrough()");
+  checkbox.addEventListener("change", function(e) {
+      var span = e.target.nextElementSibling;
+      if (e.target.checked) {
+        span.style.textDecoration = "line-through";
+      } else {
+        span.style.textDecoration = "";
+      }
+    })
   
   //add list item to html
   var itemsList = document.getElementById("itemsList");
@@ -26,21 +34,43 @@ function addToDoItem() {
 
 }
 //function for striking out text if checkbox is clicked
-function strikeThrough() {
-  var checked = document.querySelector("input[type='checkbox']:checked");
-  var unchecked = document.querySelector("input[type='checkbox']");
-  if (checked) {
-    checked.addEventListener("change", function(e) {
-      var span = e.target.nextElementSibling;
-      span.style.textDecoration="line-through"
-    })
-    console.log("a")
-  } else if(unchecked) {
-    unchecked.addEventListener("change", function(e) {
-      var span = e.target.nextElementSibling;
-      console.log(span)
-      span.style.textDecoration=""
-    })
-    console.log("b")
-  }
-}
+// function strikeThrough(e) {
+  
+//   // Add event listeners to all checkboxes when the page loads
+//   var checkboxes = document.querySelectorAll("input[type='checkbox']");
+//   checkboxes.forEach(function(checkbox) {
+//     checkbox.addEventListener("change", function(e) {
+//       var span = e.target.nextElementSibling;
+//       if (e.target.checked) {
+//         span.style.textDecoration = "line-through";
+//       } else {
+//         span.style.textDecoration = "";
+//       }
+//     });
+//   });
+
+  
+  
+  // var span = e.target.nextElementSibling;
+  //   if (e.target.checked) {
+  //     span.style.textDecoration = "line-through";
+  //   } else {
+  //     span.style.textDecoration = "";
+  //   }
+
+  // var checked = document.querySelector("input[type='checkbox']:checked");
+  // var unchecked = document.querySelector("input[type='checkbox']");
+  // if (checked) {
+  //   checked.addEventListener("change", function(e) {
+  //     var span = e.target.nextElementSibling;
+  //     span.style.textDecoration="line-through"
+  //   })
+  //   console.log("a")
+  // } else if(unchecked) {
+  //   unchecked.addEventListener("change", function(e) {
+  //     var span = e.target.nextElementSibling;
+  //     console.log(span)
+  //     span.style.textDecoration=""
+  //   })
+  //   console.log("b")
+  // }
